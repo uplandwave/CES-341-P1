33104d340004e886baaafa5e74e969c2466f377d
uplandwave <113386040+uplandwave@users.noreply.github.com>
2024-10-03 16:00:13 -0600

    validation and error handeling added.

--- routes/users.js
+++ routes/users.js
@@ -3,13 +3,24 @@ const router = express.Router();
 
 const usersControler = require('../controllers/users');
 
+const { userValidationSchema } = require('../validation/userValidation');
+
+function validateUser(req, res, next) {
+    const { error } = userValidationSchema.validate(req.body, { abortEarly: false });
+    if (error) {
+      const validationErrors = error.details.map(err => err.message);
+      return res.status(400).json({ errors: validationErrors });
+    }
+    next();
+  }
+
 router.get( '/', usersControler.getAll);
 
 router.get( '/:id', usersControler.getSingle);
   
-router.post( '/', usersControler.createUser);
+router.post('/', validateUser, usersControler.createUser);
 
-router.put( '/:id', usersControler.updateUser);
+router.put('/:id', validateUser, usersControler.updateUser);
 
 router.delete( '/:id', usersControler.deleteUser);
 
